be56b697540e135da948f3752aa7dde259b9d131
FerWeisehi <fernandoweisheim@hotmail.com>
2022-04-05 00:02:55 -0300

    de estatico a angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/header/header.component';
+import { MainComponent } from './Main/main/main.component';
+import { FooterComponent } from './Footer/footer/footer.component';
+import { NavbarComponent } from './Navbar/navbar/navbar.component';
+import { SkillsComponent } from './Skills/skills/skills.component';
+import { EducacionComponent } from './Educacion/educacion/educacion.component';
+import { BannerComponent } from './Banner/banner/banner.component';
+import { SobremiComponent } from './SobreMi/sobremi/sobremi.component';
+import { ContactoComponent } from './Contacto/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    NavbarComponent,
+    SkillsComponent,
+    EducacionComponent,
+    BannerComponent,
+    SobremiComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
